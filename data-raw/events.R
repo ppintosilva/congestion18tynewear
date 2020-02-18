9bb5e25f7da9cbee65b14b4e7335f86dc98ee786
Pedro Pinto da Silva <ppintodasilva@gmail.com>
2020-02-18 18:29:34 +0000

    Add approximate incident points to event data

--- data-raw/events.R
+++ data-raw/events.R
@@ -5,6 +5,58 @@ library(tidygraph)
 library(usethis)
 library(sf)
 
+# To be defined when adding new data ----
+
+# Event case by case filtering functions
+location_filter_functions <- list(
+  "2018-02-21" = function(x)
+    filter(x, !!quo(ref) == "A184" & !!quo(direction) == "W")
+  ,
+  "2018-03-16" = function(x)
+    filter(x, !!quo(ref) == "B1318" & !!quo(direction) == "S")
+  ,
+  "2018-05-04" = function(x)
+    filter(x, !!quo(ref) == "A690" & str_detect(!!quo(address), "Durham"))
+  ,
+  "2018-08-14" = function(x)
+    filter(x, str_detect(!!quo(address), "Angel of the North"))
+  ,
+  "2018-10-08" = function(x)
+    filter(x, !!quo(ref) == "B1318" & !!quo(direction) == "N")
+  ,
+  "2018-11-01" = function(x)
+    filter(x, !!quo(ref) == "A1018" & str_detect(!!quo(address), "Sunderland"))
+  ,
+  "2018-11-25" = function(x)
+    filter(x, (!!quo(ref) == "B1318" | !!quo(ref) == "B1307") &
+             !!quo(direction) == "S")
+  #
+  # @ADD function for new event here
+  #
+)
+
+# approximate incident location
+incident_points <- tibble::tribble(
+  ~lon, ~lat,
+  -1.537728, 54.946661,
+  -1.623651, 55.030819,
+  -1.421769, 54.885849,
+  -1.584383, 54.911493,
+  -1.621560, 55.014966,
+  -1.382897, 54.909823,
+  -1.612634, 54.979322
+  #
+  # @ADD approximate incident/congestion point for new event here
+  #
+) %>%
+  dplyr::mutate(
+    geometry = purrr::pmap(list(lon,lat),
+                           function(x,y) sf::st_point(c(x,y)))
+  ) %>%
+  tibble::rowid_to_column(var = "id") %>%
+  dplyr::select(id, geometry)
+
+
 # Functions ----
 
 gen_event_data <- function(
@@ -14,6 +66,7 @@ gen_event_data <- function(
   end_datetime,
   spatial,
   potential_sites,
+  incident_point,
   hparams = list(
     "rate_o" = .1,
     "rate_d" = .1,
@@ -66,13 +119,24 @@ gen_event_data <- function(
       bbox_margin = hparams[["bbox_margin"]]
     )
 
+  incident_point <-
+    incident_point %>%
+    sf::st_as_sf()
+
+  st_crs(incident_point) <- 4326
+
+  # project point to utm
+  incident_point <- incident_point %>%
+    sf::st_transform(crs = sf::st_crs(spatial$locations))
+
   event <- list(
     "nodes" = node_subset,
     "flows_l" = flows_l,
     "flows_od" = flows_od,
     "spatial" = subspatial,
     "network" = asympt_subnetwork,
-    "hparams" = hparams
+    "hparams" = hparams,
+    "incident" = incident_point
   )
 
   return(event)
@@ -94,33 +158,6 @@ get_potential_sites <- function(spatial, filter_function) {
     filter_function %>%
     sf::st_as_sf()
 }
-
-# To be defined when adding new data ----
-
-# Event case by case filtering functions
-location_filter_functions <- list(
-  "2018-02-21" = function(x)
-    filter(x, !!quo(ref) == "A184" & !!quo(direction) == "W")
-  ,
-  "2018-03-16" = function(x)
-    filter(x, !!quo(ref) == "B1318" & !!quo(direction) == "S")
-  ,
-  "2018-05-04" = function(x)
-    filter(x, !!quo(ref) == "A690" & str_detect(!!quo(address), "Durham"))
-  ,
-  "2018-08-14" = function(x)
-    filter(x, str_detect(!!quo(address), "Angel of the North"))
-  ,
-  "2018-10-08" = function(x)
-    filter(x, !!quo(ref) == "B1318" & !!quo(direction) == "N")
-  ,
-  "2018-11-01" = function(x)
-    filter(x, !!quo(ref) == "A1018" & str_detect(!!quo(address), "Sunderland"))
-  ,
-  "2018-11-25" = function(x)
-    filter(x, (!!quo(ref) == "B1318" | !!quo(ref) == "B1307") &
-             !!quo(direction) == "S")
-)
 
 # Main loop ----
 
@@ -155,11 +192,13 @@ events <-
   lapply(
     metadata$id,
     function(i)
-      gen_event_data(i, filenames[i],
-                     metadata$start_datetime[i],
-                     metadata$end_datetime[i],
-                     spatial,
-                     potential_sites[[i]])
+      gen_event_data(event_id = i,
+                     filename = filenames[i],
+                     start_datetime = metadata$start_datetime[i],
+                     end_datetime = metadata$end_datetime[i],
+                     spatial = spatial,
+                     potential_sites = potential_sites[[i]],
+                     incident_point = filter(incident_points, id == i))
   )
 
 usethis::use_data(events, overwrite = TRUE)
